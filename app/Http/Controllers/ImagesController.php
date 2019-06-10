7717cbb59a5fa8522287f24c981bc722370e494b
peerpower <khemmathat@peerpower.co.th>
2019-06-10 15:30:03 +0700

    Add Images: Controller, Model, Migration

---
+++ app/Http/Controllers/ImagesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ImagesModel;
+use Illuminate\Http\Request;
+
+class ImagesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ImagesModel  $photoModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ImagesModel $photoModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ImagesModel  $photoModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ImagesModel $photoModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ImagesModel  $photoModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ImagesModel $photoModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ImagesModel  $photoModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ImagesModel $photoModel)
+    {
+        //
+    }
+}
